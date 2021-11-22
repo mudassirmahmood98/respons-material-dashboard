1e68d5fe01313931a150a3a4c7dc60890b6e202e
Mudassir Mahmood <mudassirmahmood98@gmail.com>
2021-11-22 18:03:35 -0500

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,58 @@
+/*eslint-disable*/
+import React from "react";
+import PropTypes from "prop-types";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import ListItem from "@material-ui/core/ListItem";
+import List from "@material-ui/core/List";
+// core components
+import styles from "assets/jss/material-dashboard-react/components/footerStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function Footer(props) {
+  const classes = useStyles();
+  return (
+    <footer className={classes.footer}>
+      <div className={classes.container}>
+        <div className={classes.left}>
+          <List className={classes.list}>
+            <ListItem className={classes.inlineBlock}>
+              <a href="#home" className={classes.block}>
+                Home
+              </a>
+            </ListItem>
+            <ListItem className={classes.inlineBlock}>
+              <a href="#company" className={classes.block}>
+                Company
+              </a>
+            </ListItem>
+            <ListItem className={classes.inlineBlock}>
+              <a href="#portfolio" className={classes.block}>
+                Portfolio
+              </a>
+            </ListItem>
+            <ListItem className={classes.inlineBlock}>
+              <a href="#blog" className={classes.block}>
+                Blog
+              </a>
+            </ListItem>
+          </List>
+        </div>
+        <p className={classes.right}>
+          <span>
+            &copy; {1900 + new Date().getYear()}{" "}
+            <a
+              href="https://mudassirmahmood.notion.site/The-Brief-cc8fab87721f4559834afbfa2fa1d6c3"
+              target="_blank"
+              className={classes.a}
+            >
+              RESPONS— 
+            </a>
+            Made by students, for students.
+          </span>
+        </p>
+      </div>
+    </footer>
+  );
+}
